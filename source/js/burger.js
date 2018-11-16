0d4718bcc21146d7a3a33005b41bf778f0cbea43
IvanSmovzh <faceoffaceless@gmail.com>
2018-11-16 16:47:09 +0500

    Внес правки, проект закончен

--- source/js/burger.js
+++ source/js/burger.js
@@ -2,16 +2,6 @@ var btn = document.querySelector(".main-nav__btn-menu");
 var menu = document.querySelector(".main-nav__items");
 var close = document.querySelector (".main-nav__btn-indicator");
 
-// btn.addEventListener("click", function (evt) {
-//   evt.preventDefault();
-//   menu.classList.toggle("menu-open");
-// });
-
-// btn.addEventListener("click", function (evt) {
-//   evt.preventDefault();
-//   close.classList.toggle("main-nav__btn-indicator--close");
-// });
-
 document.documentElement.classList.remove("no-js");
 console.log("test");
 
--- source/js/google-cart.js
+++ source/js/google-cart.js
@@ -5,19 +5,17 @@ function initMap() {
     zoom: 16
   };
   var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-  // Добавляем маркер
   var marker = new google.maps.Marker({
-    position: centerLatLng, // Координаты расположения маркера. В данном случае координаты нашего маркера совпадают с центром карты, но разумеется нам никто не мешает создать отдельную переменную и туда поместить другие координаты.
-    // map: map,                            // Карта на которую нужно добавить маркер
-    title: "CAT ENERGY" // (Необязательно) Текст выводимый в момент наведения на маркер
+    position: centerLatLng,
+    title: "CAT ENERGY"
   });
 
   var marker = new google.maps.Marker({
-    position: centerLatLng, // Координаты расположения маркера. В данном случае координаты нашего маркера совпадают с центром карты, но разумеется нам никто не мешает создать отдельную переменную и туда поместить другие координаты.
-    map: map, // Карта на которую нужно добавить маркер
+    position: centerLatLng,
+    map: map,
     animation: google.maps.Animation.BOUNCE,
-    title: "CAT ENERGY", // (Необязательно) Текст выводимый в момент наведения на маркер,
-    icon: "img/map-pin.png" // (Необязательно) Путь до изображения, которое будет выводится на карте вместо стандартного маркера
+    title: "CAT ENERGY",
+    icon: "img/map-pin.png"
   });
 }
 google.maps.event.addDomListener(window, "load", initMap);
